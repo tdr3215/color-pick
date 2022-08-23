3d8a9d1370634da406349a14e866d01ced9387e0
Tiffany Rose <tiffany.drose@gmail.com>
2022-08-23 15:40:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import ColorPicker from './components/colorPicker/index.js';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import ColorPicker from "./components/colorPicker/index.js";
 
 const title = "Color Picker";
-
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-  
+    this.state = {
+      colorPickerOptions: this.props.colorPickerOptions,
+      initialSelectedColor: this.props.initialSelectedColor,
+      selectedColor: "",
+    };
+  }
+
+  handleClick(e) {
+    this.setState({ selectedColor: e.target.style["backgroundColor"] });
+  }
+
   render() {
     return (
       <div>
         <nav className="app-header layout-row align-items-center justify-content-center">
           <div className="layout-row align-items-center">
-            <img alt="" src={logo} className="logo"/>
-            <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
+            <img alt="" src={logo} className="logo" />
+            <h4 id="app-title" data-testid="app-title" className="app-title">
+              {title}
+            </h4>
           </div>
         </nav>
-        <ColorPicker colorPickerOptions={this.props.colorPickerOptions} initialSelectedColor={this.props.initialSelectedColor}/>
+        <ColorPicker
+          handleClick={this.handleClick.bind(this)}
+          colorPickerOptions={this.props.colorPickerOptions}
+          initialSelectedColor={this.props.initialSelectedColor}
+          selectedColor={this.state.selectedColor}
+        />
       </div>
     );
   }
--- src/components/colorPicker/index.js
+++ src/components/colorPicker/index.js
@@ -1,41 +1,50 @@
 import React, { Component } from "react";
 import "./index.css";
-const classNames = require('classnames');
+const classNames = require("classnames");
 
 export default class ColorPicker extends Component {
-  constructor(props){
-   super(props)
-
-   this.state = {colorPickerOptions:["red","black","green"],initialSelectedColor:"black"}
+  constructor(props) {
+    super(props);
   }
 
-  
-
   render() {
-    
     let selectedColor;
+    if (this.props.selectedColor === "") {
+      selectedColor = this.props.initialSelectedColor;
+    } else {
+      selectedColor = this.props.selectedColor;
+    }
 
     return (
       <div>
         <div className="layout-row justify-content-center">
           <div className="card mt-75">
-            <div className="canvas" data-testid="selectedColor">
+            <div
+              className="canvas"
+              data-testid="selectedColor"
+              style={{
+                backgroundColor: selectedColor,
+              }}
+            >
               <p className="text-center mx-auto px-5">{selectedColor}</p>
             </div>
             <div className="card-actions">
-              <div className="layout-row justify-content-center align-items-center" data-testid="colorPickerOptions">
+              <div
+                className="layout-row justify-content-center align-items-center"
+                data-testid="colorPickerOptions"
+              >
                 {this.props.colorPickerOptions.map((color, index) => {
                   return (
                     <div
-                      className={
-                        classNames({
-                          'color-box': true,
-                          'mx-8': true,
-                          'my-15': true,
-                          'selected': selectedColor === color
-                        })
-                      }
-                      key={color}
+                      style={{ backgroundColor: color }}
+                      className={classNames({
+                        "color-box": true,
+                        "mx-8": true,
+                        "my-15": true,
+                        selected: selectedColor === color,
+                      })}
+                      onClick={this.props.handleClick}
+                      key={index}
                     ></div>
                   );
                 })}
